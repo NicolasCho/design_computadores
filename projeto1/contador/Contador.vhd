2c70dd5758f39a4622e63f9bbd043e012621aea2
NicolasCho <55805364+NicolasCho@users.noreply.github.com>
2022-10-16 16:58:44 -0300

    arquitetura do contador

---
+++ projeto1/contador/Contador.vhd
@@ -0,0 +1,170 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Contador is
+  -- Total de bits das entradas e saidas
+  generic ( larguraDados : natural := 8;
+        larguraEnderecos : natural := 9;
+		  larguraInstrucao : natural := 13;
+        simulacao : boolean := TRUE -- para gravar na placa, altere de TRUE para FALSE
+  );
+  port   (
+    CLOCK_50 : in std_logic;
+	 SW    : in std_logic_vector(9 downto 0);
+	 KEY   : in std_logic_vector(3 downto 0);
+	 FPGA_RESET_N : in std_logic;
+	 
+    LEDR  : out std_logic_vector(9 downto 0);
+	 HEX0  : out std_logic_vector(6 downto 0);
+	 HEX1  : out std_logic_vector(6 downto 0);
+	 HEX2  : out std_logic_vector(6 downto 0);
+	 HEX3  : out std_logic_vector(6 downto 0);
+	 HEX4  : out std_logic_vector(6 downto 0);
+	 HEX5  : out std_logic_vector(6 downto 0);
+	 PC    : out std_logic_vector(larguraEnderecos-1 downto 0) --verificar instrucao
+  );
+end entity;
+
+architecture arquitetura of Contador is
+  signal CLK : std_logic;
+  
+  signal barramentoLeitura : std_logic_vector(larguraDados-1 downto 0);
+  
+  --sinais de I/0 da CPU
+  signal saidaROM : std_logic_vector (larguraInstrucao-1 downto 0);
+  signal saidaRAM : std_logic_vector (larguraDados-1 downto 0);
+  signal saidaDadosCPU : std_logic_vector (larguraDados-1 downto 0);
+  signal saidaEnderecosCPU : std_logic_vector (larguraEnderecos-1 downto 0);
+  signal entradaROM : std_logic_vector (larguraEnderecos-1 downto 0);
+  signal RD : std_logic;
+  signal WR : std_logic;
+  
+  --RAM
+  signal enderecoRAM : std_logic_vector (5 downto 0); 
+  
+  --sinais dos decoders de endereços e blocos
+  signal entradaDecoderBlocos : std_logic_vector (2 downto 0);
+  signal saidaDecoderBlocos : std_logic_vector (7 downto 0);
+  signal entradaDecoderEnderecos : std_logic_vector (2 downto 0);
+  signal saidaDecoderEnderecos : std_logic_vector (7 downto 0);
+  
+  --Saida de A5
+  signal divisor: std_logic;
+  
+  --LEDS
+  signal saidaLEDR : std_logic_vector(9 downto 0);
+  
+  --Displays 7 seg
+  signal saidaHex0 : std_logic_vector(6 downto 0);
+  signal saidaHex1 : std_logic_vector(6 downto 0);
+  signal saidaHex2 : std_logic_vector(6 downto 0);
+  signal saidaHex3 : std_logic_vector(6 downto 0);
+  signal saidaHex4 : std_logic_vector(6 downto 0);
+  signal saidaHex5 : std_logic_vector(6 downto 0);
+  
+  --SW
+  signal saidaSW: std_logic_vector(larguraDados-1 downto 0);
+  
+  --KEY
+  signal saidaKEY: std_logic;
+
+begin
+CLK <= CLOCK_50;
+
+--Fragmentando os enderecos
+entradaDecoderBlocos <= saidaEnderecosCPU (8 downto 6);
+entradaDecoderEnderecos <= saidaEnderecosCPU (2 downto 0);
+divisor <= saidaEnderecosCPU (5);
+enderecoRAM <= saidaEnderecosCPU (5 downto 0);
+
+--Barramento de leitura
+barramentoLeitura <= saidaRAM;
+barramentoLeitura <= saidaSW;
+barramentoLeitura(0) <= saidaKEY;
+
+-- O port map completo da CPU.
+CPU :  entity work.CPU  generic map (larguraDados => larguraDados, larguraEnderecos => larguraEnderecos, larguraInstrucao => larguraInstrucao)
+        port map( CLOCK_50 => CLK,
+					 Instruction_IN => saidaROM,
+					 Data_IN => barramentoLeitura,
+					 Data_OUT => saidaDadosCPU,	
+					 ROM_Address => entradaROM,
+					 Data_Address => saidaEnderecosCPU,
+					 RD => RD,
+					 WR => WR);
+					 
+-- O port map completo da ROM.
+ROM : entity work.memoriaROM   generic map (dataWidth => larguraInstrucao, addrWidth => larguraEnderecos)
+          port map (Endereco => entradaROM, Dado => saidaROM);
+
+-- O port map completo do decoder de blocos.			 
+DECODER_BLOCOS: entity work.decoder3x8   
+          port map (entrada => entradaDecoderBlocos, saida => saidaDecoderBlocos);
+			 
+-- O port map completo do decoder de enderecos.			 
+DECODER_ENDERECOS: entity work.decoder3x8   
+          port map (entrada => entradaDecoderEnderecos, saida => saidaDecoderEnderecos);
+
+-- O port map completo do decoder da memória RAM.	
+RAM : entity work.memoriaRAM  generic map(dataWidth => larguraDados, addrWidth => larguraEnderecos-3)
+			 port map (addr => enderecoRAM, we => WR, re => RD, 
+							habilita => saidaDecoderBlocos (0), clk => CLK, 
+							dado_in => saidaDadosCPU,
+							dado_out => saidaRAM);
+	
+-- O port map completo da lógica que ativa os LEDS.	
+LOGICALEDS : entity work.logicaLED  generic map(larguraDados => larguraDados)
+			 port map (WR => WR,
+						bloco => saidaDecoderBlocos(4),
+						endereco => saidaDecoderEnderecos(2 downto 0),
+						dados => saidaDadosCPU,
+						led_ou_display => divisor,
+						saida => saidaLEDR,
+						CLK =>CLK);
+						
+-- O port map completo da lógica que ativa os displays 7 segmentos.	
+LOGICADISPLAYS : entity work.logicaDisplay  generic map(larguraDados => larguraDados-4)
+			 port map (CLK => CLK,
+						WR => WR,
+						bloco => saidaDecoderBlocos(4),
+						endereco => saidaDecoderEnderecos(5 downto 0),
+						dados => saidaDadosCPU(3 downto 0),
+						led_ou_display => divisor,
+						hex0 => saidaHex0,
+						hex1 => saidaHex1,
+						hex2 => saidaHex2,
+						hex3 => saidaHex3,
+						hex4 => saidaHex4,
+						hex5 => saidaHex5);
+
+LOGICASW : entity work.logicaSW 
+			 port map (SW => SW,
+						RD => RD,
+						bloco => saidaDecoderBlocos(5),
+						endereco => saidaDecoderEnderecos(2 downto 0),
+						sw_ou_key => divisor,
+						saida => saidaSW);
+						
+LOGICAKEY : entity work.logicaKey
+			 port map (CLK => CLK,
+			         KEY => KEY,
+						FPGA_reset => FPGA_RESET_N,
+						RD => RD,
+						WR => WR,
+						bloco => saidaDecoderBlocos(5),
+						endereco => saidaDecoderEnderecos(4 downto 0),
+						endereco_reset => saidaEnderecosCPU,
+						sw_ou_key => divisor,
+						saida => saidaKEY);
+						
+LEDR <= saidaLEDR;					
+HEX0 <= saidaHex0;
+HEX1 <= saidaHex1;
+HEX2 <= saidaHex2;
+HEX3 <= saidaHex3;
+HEX4 <= saidaHex4;
+HEX5 <= saidaHex5;
+PC   <= entradaROM;
+
+
+end architecture;
---
+++ projeto1/contador/simulation/qsim/Waveform.vwf.vht
@@ -0,0 +1,157 @@
+-- Copyright (C) 2020  Intel Corporation. All rights reserved.
+-- Your use of Intel Corporation's design tools, logic functions 
+-- and other software and tools, and any partner logic 
+-- functions, and any output files from any of the foregoing 
+-- (including device programming or simulation files), and any 
+-- associated documentation or information are expressly subject 
+-- to the terms and conditions of the Intel Program License 
+-- Subscription Agreement, the Intel Quartus Prime License Agreement,
+-- the Intel FPGA IP License Agreement, or other applicable license
+-- agreement, including, without limitation, that your use is for
+-- the sole purpose of programming logic devices manufactured by
+-- Intel and sold by Intel or its authorized distributors.  Please
+-- refer to the applicable agreement for further details, at
+-- https://fpgasoftware.intel.com/eula.
+
+-- *****************************************************************************
+-- This file contains a Vhdl test bench with test vectors .The test vectors     
+-- are exported from a vector file in the Quartus Waveform Editor and apply to  
+-- the top level entity of the current Quartus project .The user can use this   
+-- testbench to simulate his design using a third-party simulation tool .       
+-- *****************************************************************************
+-- Generated on "10/14/2022 11:23:06"
+                                                             
+-- Vhdl Test Bench(with test vectors) for design  :          Contador
+-- 
+-- Simulation tool : 3rd Party
+-- 
+
+LIBRARY ieee;                                               
+USE ieee.std_logic_1164.all;                                
+
+ENTITY Contador_vhd_vec_tst IS
+END Contador_vhd_vec_tst;
+ARCHITECTURE Contador_arch OF Contador_vhd_vec_tst IS
+-- constants                                                 
+-- signals                                                   
+SIGNAL barr_leit : STD_LOGIC_VECTOR(7 DOWNTO 0);
+SIGNAL CLOCK : STD_LOGIC;
+SIGNAL FPGA_RESET : STD_LOGIC;
+SIGNAL HEX0 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL HEX1 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL HEX2 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL HEX3 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL HEX4 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL HEX5 : STD_LOGIC_VECTOR(6 DOWNTO 0);
+SIGNAL KEY0 : STD_LOGIC;
+SIGNAL KEY1 : STD_LOGIC;
+SIGNAL KEY2 : STD_LOGIC;
+SIGNAL KEY3 : STD_LOGIC;
+SIGNAL LEDR : STD_LOGIC_VECTOR(9 DOWNTO 0);
+SIGNAL PC : STD_LOGIC_VECTOR(8 DOWNTO 0);
+SIGNAL saida_disc : STD_LOGIC;
+SIGNAL saida_ff : STD_LOGIC;
+SIGNAL saida_hab : STD_LOGIC;
+SIGNAL SW0 : STD_LOGIC;
+SIGNAL SW1 : STD_LOGIC;
+SIGNAL SW2 : STD_LOGIC;
+SIGNAL SW3 : STD_LOGIC;
+SIGNAL SW4 : STD_LOGIC;
+SIGNAL SW5 : STD_LOGIC;
+SIGNAL SW6 : STD_LOGIC;
+SIGNAL SW7 : STD_LOGIC;
+SIGNAL SW8 : STD_LOGIC;
+SIGNAL SW9 : STD_LOGIC;
+COMPONENT Contador
+	PORT (
+	barr_leit : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+	CLOCK : IN STD_LOGIC;
+	FPGA_RESET : IN STD_LOGIC;
+	HEX0 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	HEX1 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	HEX2 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	HEX3 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	HEX4 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	HEX5 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	KEY0 : IN STD_LOGIC;
+	KEY1 : IN STD_LOGIC;
+	KEY2 : IN STD_LOGIC;
+	KEY3 : IN STD_LOGIC;
+	LEDR : OUT STD_LOGIC_VECTOR(9 DOWNTO 0);
+	PC : OUT STD_LOGIC_VECTOR(8 DOWNTO 0);
+	saida_disc : OUT STD_LOGIC;
+	saida_ff : OUT STD_LOGIC;
+	saida_hab : OUT STD_LOGIC;
+	SW0 : IN STD_LOGIC;
+	SW1 : IN STD_LOGIC;
+	SW2 : IN STD_LOGIC;
+	SW3 : IN STD_LOGIC;
+	SW4 : IN STD_LOGIC;
+	SW5 : IN STD_LOGIC;
+	SW6 : IN STD_LOGIC;
+	SW7 : IN STD_LOGIC;
+	SW8 : IN STD_LOGIC;
+	SW9 : IN STD_LOGIC
+	);
+END COMPONENT;
+BEGIN
+	i1 : Contador
+	PORT MAP (
+-- list connections between master ports and signals
+	barr_leit => barr_leit,
+	CLOCK => CLOCK,
+	FPGA_RESET => FPGA_RESET,
+	HEX0 => HEX0,
+	HEX1 => HEX1,
+	HEX2 => HEX2,
+	HEX3 => HEX3,
+	HEX4 => HEX4,
+	HEX5 => HEX5,
+	KEY0 => KEY0,
+	KEY1 => KEY1,
+	KEY2 => KEY2,
+	KEY3 => KEY3,
+	LEDR => LEDR,
+	PC => PC,
+	saida_disc => saida_disc,
+	saida_ff => saida_ff,
+	saida_hab => saida_hab,
+	SW0 => SW0,
+	SW1 => SW1,
+	SW2 => SW2,
+	SW3 => SW3,
+	SW4 => SW4,
+	SW5 => SW5,
+	SW6 => SW6,
+	SW7 => SW7,
+	SW8 => SW8,
+	SW9 => SW9
+	);
+
+-- CLOCK
+t_prcs_CLOCK: PROCESS
+BEGIN
+	CLOCK <= '1';
+	WAIT FOR 20000 ps;
+	FOR i IN 1 TO 24
+	LOOP
+		CLOCK <= '0';
+		WAIT FOR 20000 ps;
+		CLOCK <= '1';
+		WAIT FOR 20000 ps;
+	END LOOP;
+	CLOCK <= '0';
+WAIT;
+END PROCESS t_prcs_CLOCK;
+
+-- KEY0
+t_prcs_KEY0: PROCESS
+BEGIN
+	KEY0 <= '0';
+	WAIT FOR 310000 ps;
+	KEY0 <= '1';
+	WAIT FOR 120000 ps;
+	KEY0 <= '0';
+WAIT;
+END PROCESS t_prcs_KEY0;
+END Contador_arch;
