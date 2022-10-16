44a8e694f09145ebc5499d421e7e274919ac3f33
BrunoFNRodrigues <bf17669@gmail.com>
2022-10-16 18:11:33 -0300

    assembler mais interessante

--- assembler/asm.py
+++ assembler/asm.py
@@ -1,9 +1,9 @@
 OpCode = { "NOP":"0000", "LDA":"0001", "SOMA":"0010", "SUB":"0011", "LDI":"0100", "STA":"0101",
            "JMP":"0110", "JEQ":"0111", "CEQ":"1000", "JSR":"1001", "RET":"1010", "ANDOP":"1011"}
 
-# MemoryMap = {"RAM":range(0,63), "LEDR0-7": 256, "LEDR8":257, "LEDR9":258, "HEX:0":288, "HEX1":289, "HEX2":290,
-#              "HEX3":291, "HEX4":292, "HEX5":293, "SW0-7":320, "SW8":321, "SW9":322, "KEY0":352, "KEY1":353, 
-#              "KEY2":354, "KEY3":355, "FPGA_RESET":356, "CLR-KEY1":510, "CLR-KEY2":511}
+MemoryMap = {"LEDR0-7": "@256", "LEDR8":"@257", "LEDR9":"@258", "HEX0":"@288", "HEX1":"@289", "HEX2":"@290",
+             "HEX3":"@291", "HEX4":"@292", "HEX5":"@293", "SW0-7":"@320", "SW8":"@321", "SW9":"@322", "KEY0":"@352", "KEY1":"@353", 
+             "KEY2":"@354", "KEY3":"@355", "FPGA_RESET":"@356", "CLR-KEY1":"@510", "CLR-KEY2":"@511"}
 
 lines, lineinfo, lineadr, labels = [], [], [], {}
 
@@ -28,8 +28,8 @@ for i in range(0,len(lines)):
     for k,d in OpCode.items():
         line = line.replace(k, d) #Passa os minemonicos para o código binário
 
-    # for k,d in MemoryMap.items():
-    #     line = line.replace(k, bin(int(d))) #Passa os minemonicos para o código binário
+    for k,d in MemoryMap.items():
+        line = line.replace(k, d) #Passa os minemonicos para o código binário
 
     if "@" in line:
         num = line.split("@",1)[1]
